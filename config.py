bdfb09229e226b28c70c339c7a0b5196589a8a39
Karl <63649538+StasonJatham@users.noreply.github.com>
2020-04-14 16:32:15 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+toolsdict = {
+    "sqlmap": "Pwn some sql",
+    "dnsdumpster":"DNS Enumeration",
+    "bettercap": "Pwn some WIFI",
+    "dir": "Like ls - just in boring",
+    "ipconfig": "Show some nerd network stuff",
+    "python": "Run awesome shit"}
+
+target = {"url":"example.de"}
